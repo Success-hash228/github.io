5730b41a6c0410f8905e015bd097088aed4dbe48
Success-hash228 <oloyedesuccess64@gmail.com>
2025-09-09 10:31:01 +0100

    Add files via upload
    Added portfolio files

---
+++ PORTFOLIO/main.bat
@@ -0,0 +1,11 @@
+source /home/ubuntu/.user_env && cd /home/ubuntu && mkdir -p portfolio-website && cd portfolio-website && mkdir -p assets/images assets/css assets/js
+
+source /home/ubuntu/.user_env && cd /home/ubuntu/portfolio-website && cp /home/ubuntu/upload/ChatGPTImageSep6,2025,12_07_19PM.png /home/ubuntu/portfolio-website/assets/images/profile-photo.png
+
+REM Create the destination folder if it doesn't exist
+mkdir "C:\Users\OLOYEDE SUCCESS\OneDrive\Documenti\local-server-app\public\PORTFOLIO\assets\images"
+
+REM Copy the headshot.jpg.png file to the images folder
+copy "C:\Users\OLOYEDE SUCCESS\Downloads\headshot.jpg.png" "C:\Users\OLOYEDE SUCCESS\OneDrive\Documenti\local-server-app\public\PORTFOLIO\assets\images\headshot.jpg.png"
+
+cd "C:\Users\OLOYEDE SUCCESS\OneDrive\Documenti\local-server-app\public\PORTFOLIO\assets\images"
